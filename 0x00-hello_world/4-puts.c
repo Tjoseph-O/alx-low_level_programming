5bb83a81fc638dbcd2c79ee39f5cc0bca9044eb8
Temitope Joseph <mrtemitopejoseph@gmail.com>
2023-07-16 04:21:24 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main A program that prints a line with print function;
- * Return; 0
+*main -A program that prints a line with put function;
+*Return: 0 
+*/	
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
